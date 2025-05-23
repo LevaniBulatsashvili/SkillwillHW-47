e98952ab21359761671c9b1654b7c3ce74915d93
Levani Bulatsashvili <levangmail.com>
2025-05-23 08:59:09 +0400

    rm: unneccessary code

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,5 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "../components/TabNavigator";
 import WhatsAppHeader from "../components/WhatsAppHeader";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
